33725314bde55625166b2113cfb8020fd0a3a221
Robbie Zuazua <rzuazua@utexas.edu>
2020-08-30 16:32:47 -0500

    add q6 java code

---
+++ Strings/Q6. Reverse Word Ordering/ReverseWordOrdering.java
@@ -0,0 +1,21 @@
+public class ReverseWordOrdering {
+    public static void main(String[] args) {
+        // Change these values to manually test your program
+        final String input = "rob likes code";
+        final String expectedResponse = "code likes rob";
+        final String actualResponse = reverseWordOrdering(input);
+
+        LogUtil.logResults(expectedResponse, actualResponse);
+    }
+
+    /**
+     * Write your code here 
+     * 
+     * Reverse all the words such that the words appear in reverse order. Trim Whitespace. (See README for details) 
+     * @param s - input string containing either alphnumeric characters and/or whitespace
+     * @return the words in reverse order
+     */
+    public static String reverseWordOrdering(String s) {
+        return s;
+    }
+}
